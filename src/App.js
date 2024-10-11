f7df0fd36d3cb85e7a76a63081ed4c1cd8e28ef5
[nyKim] <[harb0719@naver.com]>
2024-10-11 17:14:28 +0900

    Apply Material UI

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
 import Customer from './components/Customer';
+import { styled } from '@mui/system';
+import { Paper, Table, TableHead, TableBody, TableRow, TableCell } from '@mui/material';
+
+const Root = styled('div')(({ theme }) => ({
+  width: '100%',
+  marginTop: theme.spacing(3),
+  overflowX: 'auto',
+}));
+
+const StyledTable = styled(Table)({
+  minWidth: 1080,
+});
 
 const customers = [
   {
@@ -29,14 +41,39 @@ const customers = [
   }
 ]
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        { customers.map(c => { return ( <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />); }) }
-      </div>
-    );
-  }
+function App() {
+  return (
+    <Root>
+      <Paper>
+        <StyledTable>
+          <TableHead>
+            <TableRow>
+              <TableCell>번호</TableCell>
+              <TableCell>이미지</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>생년월일</TableCell>
+              <TableCell>성별</TableCell>
+              <TableCell>직업</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {customers.map((c) => (
+              <TableRow key={c.id}>
+                <TableCell>{c.id}</TableCell>
+                <TableCell>
+                  <img src={c.image} alt={c.name} />
+                </TableCell>
+                <TableCell>{c.name}</TableCell>
+                <TableCell>{c.birthday}</TableCell>
+                <TableCell>{c.gender}</TableCell>
+                <TableCell>{c.job}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </StyledTable>
+      </Paper>
+    </Root>
+  );
 }
 
 export default App;
