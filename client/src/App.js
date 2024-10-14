2eb116a54b547baa22af8b395a104e0b98bed04d
[nyKim] <[harb0719@naver.com]>
2024-10-14 17:41:01 +0900

    Add Get Customers REST API Module

--- client/src/App.js
+++ client/src/App.js
@@ -14,66 +14,59 @@ const StyledTable = styled(Table)({
   minWidth: 1080,
 });
 
-const customers = [
-  {
-    'id' : 1,
-    'image' : 'https://placeimg.com/64/64/1',
-    'name' : '홍길동',
-    'birthday' : '960911',
-    'gender' : '남자',
-    'job' : '대학생'
-  },
-  {
-    'id' : 2,
-    'image' : 'https://placeimg.com/64/64/2',
-    'name' : '춘향',
-    'birthday' : '990312',
-    'gender' : '여자',
-    'job' : '프로그래머'
-  },
-  {
-    'id' : 3,
-    'image' : 'https://placeimg.com/64/64/3',
-    'name' : '누군가',
-    'birthday' : '000000',
-    'gender' : '남자',
-    'job' : '교수'
-  }
-]
+class App extends Component {
 
-function App() {
-  return (
-    <Root>
-      <Paper>
-        <StyledTable>
-          <TableHead>
-            <TableRow>
-              <TableCell>번호</TableCell>
-              <TableCell>이미지</TableCell>
-              <TableCell>이름</TableCell>
-              <TableCell>생년월일</TableCell>
-              <TableCell>성별</TableCell>
-              <TableCell>직업</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {customers.map((c) => (
-              <TableRow key={c.id}>
-                <TableCell>{c.id}</TableCell>
-                <TableCell>
-                  <img src={c.image} alt={c.name} />
-                </TableCell>
-                <TableCell>{c.name}</TableCell>
-                <TableCell>{c.birthday}</TableCell>
-                <TableCell>{c.gender}</TableCell>
-                <TableCell>{c.job}</TableCell>
+  state = {
+    customers:""
+  }
+
+  componentDidMount(){
+    this.callApi()
+    .then(res => this.setState({customers:res}))
+    .catch(err => console.log(err));
+  }
+
+  callApi = async() => {
+    const response = await fetch('/api/customers');
+    const body = await response.json();
+    return body;
+  }
+
+  render() {
+    const {classes } = this.props;
+    return (
+      <Root>
+        <Paper>
+          <StyledTable>
+            <TableHead>
+              <TableRow>
+                <TableCell>번호</TableCell>
+                <TableCell>이미지</TableCell>
+                <TableCell>이름</TableCell>
+                <TableCell>생년월일</TableCell>
+                <TableCell>성별</TableCell>
+                <TableCell>직업</TableCell>
               </TableRow>
-            ))}
-          </TableBody>
-        </StyledTable>
-      </Paper>
-    </Root>
-  );
+            </TableHead>
+            <TableBody>
+              {this.state.customers ? this.state.customers.map((c) => (
+                <TableRow key={c.id}>
+                  <TableCell>{c.id}</TableCell>
+                  <TableCell>
+                    <img src={c.image} alt={c.name} />
+                  </TableCell>
+                  <TableCell>{c.name}</TableCell>
+                  <TableCell>{c.birthday}</TableCell>
+                  <TableCell>{c.gender}</TableCell>
+                  <TableCell>{c.job}</TableCell>
+                </TableRow>
+              )) : ""}
+            </TableBody>
+          </StyledTable>
+        </Paper>
+      </Root>
+    );
+  }
 }
 
 export default App;
--- server.js
+++ server.js
@@ -4,10 +4,35 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.urlencoded({extended:true}));
 
-app.get('/api/hello', (req,res)=>{
-    res.send({message: 'Hello Express!'});
+app.get('/api/customers', (req,res)=>{
+    res.send([
+        {
+          'id' : 1,
+          'image' : 'https://placeimg.com/64/64/1',
+          'name' : '홍길동',
+          'birthday' : '960911',
+          'gender' : '남자',
+          'job' : '대학생'
+        },
+        {
+          'id' : 2,
+          'image' : 'https://placeimg.com/64/64/2',
+          'name' : '춘향',
+          'birthday' : '990312',
+          'gender' : '여자',
+          'job' : '프로그래머'
+        },
+        {
+          'id' : 3,
+          'image' : 'https://placeimg.com/64/64/3',
+          'name' : '누군가',
+          'birthday' : '000000',
+          'gender' : '남자',
+          'job' : '교수'
+        }
+      ])
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
